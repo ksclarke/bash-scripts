a586b52a026b4fd538dd919f15ad5d2168d3590b
Kevin S. Clarke <ksclarke@gmail.com>
2014-05-23 13:07:43 -0400

    removed obsolete pidgin wrapper

--- pidgin.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-#===========================================================================
-# A script to wrap Pidgin, resolving its Microsoft Lync compatibility issue
-#===========================================================================
-
-export NSS_SSL_CBC_RANDOM_IV=0
-/usr/bin/pidgin
